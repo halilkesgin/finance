ce7dee5c3e49331f31e9b0a1f74f855c95cf3578
Halil <halillkesgin@gmail.com>
2024-07-15 22:59:02 +0300

    tanstack, hono post api handler done

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,29 +1,54 @@
 import { Hono } from "hono"
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth"
-import { HTTPException } from "hono/http-exception"
 import { eq } from "drizzle-orm"
+import { zValidator } from "@hono/zod-validator"
+import { createId } from "@paralleldrive/cuid2"
 
 import { db } from "@/db/drizzle"
-import { accounts } from "@/db/schema"
+import { accounts, insertAccountSchema } from "@/db/schema"
 
 const app = new Hono()
-    .get("/", clerkMiddleware(), async (ctx) => {
-        const auth = getAuth(ctx)
+    .get(
+        "/", 
+        clerkMiddleware(), 
+        async (ctx) => {
+            const auth = getAuth(ctx)
 
-        if (!auth?.userId) {
-            throw new HTTPException(401, {
-                res: ctx.json({ error: "Unauthorized." }, 401)
-            })
-        }
+            if (!auth?.userId) {
+                return ctx.json({ error: "Unauthorized." }, 401)
+            }
         
-        const data = await db.select({
-            id: accounts.id,
-            name: accounts.name
-        })
-        .from(accounts)
-        .where(eq(accounts.userId, auth.userId))
+            const data = await db.select({
+                id: accounts.id,
+                name: accounts.name
+            })
+            .from(accounts)
+            .where(eq(accounts.userId, auth.userId))
 
-        return ctx.json({ data })
-    })
+            return ctx.json({ data })
+        })
+
+    .post(
+        "/", 
+        clerkMiddleware(), 
+        zValidator("json", insertAccountSchema.pick({
+            name: true
+        })), 
+        async (ctx) => {
+            const auth = getAuth(ctx)
+            const values = ctx.req.valid("json")
+
+            if (!auth?.userId) {
+                return ctx.json({ error: "Unauthorized." }, 401)
+            }
+
+            const data = await db.insert(accounts).values({
+                id: createId(),
+                userId: auth.userId,
+                ...values
+            }).returning()
+
+            return ctx.json({ data })
+        })
 
 export default app
