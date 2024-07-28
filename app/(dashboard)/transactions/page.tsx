8ba74f2e9fc77604c52feed5d4f092db0c04ffc7
halilkesgin <halillkesgin@gmail.com>
2024-07-28 13:05:19 +0300

    transaction import done

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Loader2, Plus } from "lucide-react";
+import { useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -11,14 +12,41 @@ import { useGetTransactions } from "@/features/transactions/api/use-get-transact
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
 
 import { columns } from "./columns";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+
+enum VARIANTS {
+    LIST = "LIST",
+    IMPORT = "IMPORT",
+}
+
+const INITIAL_IMPORT_RESULTS = {
+    data: [],
+    errors: [],
+    meta: {},
+};
 
 const TransactionsPage = () => {
+    const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+    const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+    const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+        setImportResults(results);
+        setVariant(VARIANTS.IMPORT);
+    };
+
+    const onCancelImport = () => {
+        setImportResults(INITIAL_IMPORT_RESULTS);
+        setVariant(VARIANTS.LIST);
+    };
+
     const newTransaction = useNewTransaction();
     const deleteTransactions = useBulkDeleteTransactions();
     const transactionsQuery = useGetTransactions();
     const transactions = transactionsQuery.data || [];
 
-    const isDisabled = transactionsQuery.isLoading || deleteTransactions.isPending;
+    const isDisabled =
+        transactionsQuery.isLoading || deleteTransactions.isPending;
 
     if (transactionsQuery.isLoading) {
         return (
@@ -30,10 +58,22 @@ const TransactionsPage = () => {
                     <CardContent>
                         <div className="h-[500px] w-full flex items-center justify-center">
                             <Loader2 className="size-6 text-slate-300 animate-spin" />
-                        </div> 
+                        </div>
                     </CardContent>
                 </Card>
             </div>
+        );
+    }
+
+    if (variant === VARIANTS.IMPORT) {
+        return (
+            <>
+                <ImportCard
+                    data={importResults.data}
+                    onCancel={onCancelImport}
+                    onSubmit={() => {}}
+                />
+            </>
         );
     }
 
@@ -44,10 +84,13 @@ const TransactionsPage = () => {
                     <CardTitle className="text-xl line-clamp-1">
                         Transaction History
                     </CardTitle>
-                    <Button size="sm" onClick={newTransaction.onOpen}>
-                        <Plus className="size-4 mr-2" />
-                        Add new
-                    </Button>
+                    <div className="flex flex-col lg:flex-row gap-y-2 items-center gap-x-2">
+                        <Button size="sm" onClick={newTransaction.onOpen} className="w-full lg:w-auto">
+                            <Plus className="size-4 mr-2" />
+                            Add new
+                        </Button>
+                        <UploadButton onUpload={onUpload} />
+                    </div>
                 </CardHeader>
                 <CardContent>
                     <DataTable
